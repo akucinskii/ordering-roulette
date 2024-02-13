b83f3c61ee29d57bbf61ce65796b633986c42d4a
Artur Kucinski <artuyees@gmail.com>
2024-02-13 23:17:10 +0100

    added component to list previous lotteries

--- src/screens/RoomScreen.tsx
+++ src/screens/RoomScreen.tsx
@@ -1,15 +1,42 @@
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
-import React, { useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { TextInput, View } from "react-native";
 import { generateCode } from "../utils/generateCode";
 import { RootStackParamList } from "../types";
 import { Button, FormControl, Input, Text } from "native-base";
+import { getData } from "../utils/storage";
+import { useFocusEffect } from "@react-navigation/native";
 
 type RoomScreenNavigationProp = NativeStackNavigationProp<RootStackParamList, "Room">;
+
+type Lottery = {
+  id: number;
+  date: string;
+  room: string;
+  participants: string;
+  winner: string;
+};
 
 const RoomScreen = ({ navigation }: { navigation: RoomScreenNavigationProp }) => {
   const [roomCode, setRoomCode] = useState<string>("");
   const [error, setError] = useState<string>("");
+  const [oldLotteries, setOldLotteries] = useState<Lottery[]>([]);
+
+  useFocusEffect(
+    useCallback(() => {
+      const getLotteries = async () => {
+        const username = await getData("username");
+
+        fetch(`http://localhost:3000/lotteries/${username}`)
+          .then((response) => response.json())
+          .then((data) => {
+            setOldLotteries(data);
+          });
+      };
+      getLotteries();
+    }, [])
+  );
+  console.log("e/");
 
   const handleCreateRoom = () => {
     const code = generateCode();
@@ -38,12 +65,12 @@ const RoomScreen = ({ navigation }: { navigation: RoomScreenNavigationProp }) =>
       }}
     >
       <FormControl isInvalid={!!error} w="75%" maxW="300px">
-        <FormControl.Label>Room Code</FormControl.Label>
+        <FormControl.Label>Enter room code provided by friend</FormControl.Label>
         <Input
           onChangeText={(text) => {
             setRoomCode(text);
           }}
-          placeholder="Enter room name"
+          placeholder="Enter room code"
         ></Input>
         <FormControl.ErrorMessage>{error}</FormControl.ErrorMessage>
       </FormControl>
@@ -56,6 +83,13 @@ const RoomScreen = ({ navigation }: { navigation: RoomScreenNavigationProp }) =>
       <Button w="75%" maxW="300px" onPress={handleCreateRoom}>
         Create new Room
       </Button>
+
+      <Text>Your previous lotteries:</Text>
+      {oldLotteries.map((lottery) => (
+        <Text key={lottery.id}>
+          {new Date(lottery.date).toLocaleString()}: {lottery.winner}
+        </Text>
+      ))}
     </View>
   );
 };
